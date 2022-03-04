f2f9771ac8386ea69349f33fa56eaecd9dbd543e
Sebnem Turkoglu <sebnem.turkoglu@ug.bilkent.edu.tr>
2022-03-04 10:51:41 +0300

    add evaluation code for Multiply

--- Main.java
+++ Main.java
@@ -11,4 +11,7 @@ public class Main {
     // Try subtraction
     calculator.Subtract(50, 15);
     System.out.println(calculator.getLastResult());
+    // Try Multiplication
+    calculator.Multiply(10, 30);
+    System.out.println(calculator.getLastResult());
 }
